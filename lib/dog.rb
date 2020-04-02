4f77cf3a42e20245b80bec9e08b6420f5ff47ec5
Javante Nelson <javantenelson@gmail.com>
2020-04-02 22:46:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   @@all = []
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
   
   def self.all
@@ -20,7 +20,7 @@ class Dog
   end
   
   def save
-    
+    @@all << self
   end
   
   def name
